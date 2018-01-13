8e42113011dc94e3ab572e30cb26823e2b8b6341
mahavirvataliya <mahavir.vataliya110@gmail.com>
2018-01-13 15:07:11 +0530

    new laravel project1

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -5,9 +5,9 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">My Notes
-
-
+                    <div class="panel-heading">My Notes <a class="btn btn-success" href="{{ url('create') }}">
+                           &nbsp; <i class="fa fa-plus" aria-hidden="true"></i>
+                         Create Note</a>
                     </div>
                     <div class="panel-body">
                         <?php 
