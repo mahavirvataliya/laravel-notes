b6e8616d1662e4a214dd104cd35f3a5d4ccefa1e
mahavirvataliya <mahavir.vataliya110@gmail.com>
2018-01-14 04:17:50 +0530

    Final

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,27 +1,56 @@
 @extends('layouts.app')
 @section('content')
+<style type="text/css">
+    
+
+</style>
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">About Me 
+                    <div class="panel-heading">About Developer
                     </div>
-                    <div class="panel-body">
-                        <h4>Mahavir Vataliya</h4>
+                    <div class="panel-body text-center">
+                        <h4>Created By Mahavir Vataliya</h4>
+                        <div class="card" >
+                          <img width="25%" class="card-img-top " src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAwDGAAwAAQAAAAAAAAroAAAAJGU0OGFmZDQwLTliZjctNGU0Zi05MjM4LTA2M2ZlNDlmNDg4MA.jpg" alt="Card image">
+                          <div class="card-body">
+                            <h4 class="card-title">Mahavir Vataliya</h4>
+                           <p class="card-text"><h4>Email <i class="fa fa-envelope"></i></h4>
+                    <a href="mailto:mahavir.vataliya110@gmail.com?body=From Share Note Application Feedback" type="button" class="btn btn-large"> mahavir.vataliya110@gmail.com </a>
+                    <h4>Mobile Number <i class="fa fa-phone"></i></h4>
+                        +91 8140257443
+                </p>
+                          </div>
+                        </div>
+
+
                     </div>
                 </div>
                 <div class="panel panel-default">
-                    <div class="panel-heading">Shared Notes With Me</div>
-                    <div class="panel-body">
-                    
+                    <div class="panel-heading">Social Deteails</div>
+                    <div class="panel-body text-center">
+                   <!--Facebook-->
+                    <a href="https://www.facebook.com/mahavir.vataliya" target="_blank" class="btn btn-primary btn-large">
+                        <i class="fa fa-2x fa-facebook"></i></a>
+                        &nbsp;
+                    <!--Twitter-->
+                    <a href="https://twitter.com/mahavirvataliya" target="_blank" class="btn btn-large btn-primary"><i class="fa fa-2x fa-twitter"></i></a>
+                    <!--Google +-->&nbsp;
+                    <a type="button" href="http://google.com/+MahavirVataliya" target="_blank" class="btn-danger btn btn-large"><i class="fa fa-2x fa-google-plus"></i></a>
+                    <!--Linkedin-->&nbsp;
+                    <a type="button" href="https://www.linkedin.com/in/mahavir-vataliya-1ab134141" target="_blank" class="btn btn-primary btn-large"><i class="fa fa-2x fa-linkedin"></i></a>
+                   
+                    <!--Youtube-->&nbsp;
+                    <a type="button" href="https://www.youtube.com/channel/UCqmYeVDas69YjXuVsuNB8Rw" target="_blank" class="btn btn-danger btn-large"><i class="fa fa-2x fa-youtube"></i></a>
+          
+                   
+                    <!--Github-->&nbsp;
+                    <a type="button" href="https://github.com/mahavirvataliya" href="_blank" class="btn btn-large btn-primary "><i class="fa fa-2x fa-github"></i></a>
+
                     </div>
                 </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">All Tags</div>
-                    <div class="panel-body">
-                    
-                    </div>
-                </div>
+               
             </div>
         </div>
     </div>
