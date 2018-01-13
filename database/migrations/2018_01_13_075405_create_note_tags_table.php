1bf17800eae4ca64c049952cb1f0d4b9e1ab5b0a
mahavirvataliya <mahavir.vataliya110@gmail.com>
2018-01-13 14:32:49 +0530

    new laravel project

---
+++ database/migrations/2018_01_13_075405_create_note_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoteTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tag_id');
+            $table->integer('note_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_tags');
+    }
+}
